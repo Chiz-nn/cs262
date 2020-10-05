01c05642021584b359a53ad7a31f72071f5709dd
Chiz-nn <cin2@students.calvin.edu>
2020-10-05 01:15:38 -0400

    lab04

--- lab04/gamezone/App.js
+++ lab04/gamezone/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import Navigator from './routes/homeStack';
-
+//set parameters
 const getFonts = () => Font.loadAsync({
 	'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
 	'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
